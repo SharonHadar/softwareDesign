e5ca82de417d5736e16e935e40dbc104a4c3bea1
SharonHadar <sharonhadar@campus.technion.ac.il>
2018-04-16 16:50:35 +0300

    library api

---
+++ ConfessSkeleton/base/library/src/main/java/SecureDataBaseWrapper.java
@@ -0,0 +1,22 @@
+package base.library.src.main.java.foo;
+
+public class SecureDataBaseWrapper{
+
+    SecureDataBaseWrapper(){
+
+    }
+
+    public static void addEntry(String key, String value){
+        
+    }
+
+    public static String get(String key){
+
+    }
+
+    /*get the entries in the CSV format. each line is an entry with the pattern
+    * key,value*/
+    public static addEntries(String entries){
+
+    }
+}
